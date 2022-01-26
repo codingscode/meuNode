398770023081aa1dadaf54e79af80a83dc791280
codingscode <heronnestg@hotmail.com>
2022-01-25 22:22:41 -0300

    parte 5

---
+++ curso/001/file006.js
@@ -0,0 +1,15 @@
+
+const importado = require('./file006.utils')
+
+console.log(importado.nome)
+console.log(importado.adicionar(1,2))
+
+
+
+// node file006.js
+
+/*
+utils.js
+Apolo
+3
+*/
---
+++ curso/001/file006.utils.js
@@ -0,0 +1,12 @@
+
+console.log('utils.js')
+
+const nome = 'Apolo'
+
+
+const adicionar = (a, b) => {
+   return a + b
+}
+
+
+module.exports = { nome, adicionar } //ou { nome: nome, adicionar: adicionar }
