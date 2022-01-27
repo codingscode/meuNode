9ff2d5f0d3eeac3bc0c549a6262b15ce564a234a
codingscode <heronnestg@hotmail.com>
2022-01-27 13:08:38 -0300

    parte 8

---
+++ curso/002/file002.js
@@ -0,0 +1,15 @@
+import os from 'os' //  const os = require("os")
+
+import chalk from 'chalk'//const chalk = require("chalk")
+
+
+const mensagemVerde = chalk.bold.magenta('Sucesso!')
+
+console.log(mensagemVerde)
+console.log(chalk.blue.bold('Sucesso outro!'))
+console.log(chalk.blue.inverse.bold('Sucesso outro!'))
+
+
+
+
+
