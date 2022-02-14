4e9eefa964829906fa6a924961c28ebe6dc808a5
codingscode <heronnestg@hotmail.com>
2022-02-14 09:11:09 -0300

    parte 24

---
+++ curso/002/file014_app.js
@@ -0,0 +1,73 @@
+
+
+const notas = require('./file014_nota.js')
+
+const yargs = require('yargs')
+
+
+// customizar yargs
+yargs.version('1.1.0')
+
+// criar comando listar
+yargs.command({
+   command: 'listar',
+   describe: 'Listar suas notas',
+   builder: {
+      titulo: {
+         describe: 'Titulo da nota',
+         demandOption: true,
+         type: 'string'
+      },
+      corpo: {
+         describe: 'Nota corpo',
+         demandOption: true,
+         type: 'string'
+      }
+   },
+   handler: (argv) => {
+      notas.adicionarNota(argv.titulo, argv.corpo)
+   }
+})
+
+//criar comando ler
+yargs.command({
+   command: 'ler',
+   describe: 'Ler suas notas',
+   handler: () => {
+      console.log('Lendo suas notas')
+   }
+})
+
+
+// adicionar, remover, ler, listar
+
+
+
+
+
+console.log(process.argv)
+
+yargs.parse()
+//console.log(yargs.argv)
+
+
+// node file014_app.js listar --titulo="t" --corpo="c"
+/* 
+
+
+*/
+
+// node file014_app.js listar --titulo="comida" --corpo="hamburguer"
+
+// comando repetido node file014_app.js listar --titulo="comida" --corpo="hamburguer"
+
+// comando nao repetido node file014_app.js listar --titulo="eletronicos" --corpo="pendrive"
+
+
+
+
+
+
+
+
+
---
+++ curso/002/file014_nota.js
@@ -0,0 +1,54 @@
+
+const fs = require('fs')
+
+
+const acessarNotas = () => {
+   return 'Suas notas...'
+}
+
+const adicionarNota = (titulo, corpo) => {
+
+   const notas = carregarNotas()
+   
+   const notasDuplicadas = notas.filter((nota) => {
+      return nota.titulo === titulo
+   })
+   
+   if (notasDuplicadas.length === 0) {
+      notas.push({ titulo: titulo, corpo: corpo })
+      
+      console.log('nova nota adicionada!')
+   }
+   else {
+      console.log('Nota titulo ocupada!')
+   }
+   
+   
+   
+   salvarNotas(notas)
+}
+
+const salvarNotas = (notas) => {
+   const dadosJSON = JSON.stringify(notas)
+   fs.writeFileSync('file014notas.json', dadosJSON)
+}
+
+
+const carregarNotas = () => {
+
+   try {
+      const dadosBuffer = fs.readFileSync('file014notas.json')
+      const dadosJSON = dadosBuffer.toString()
+      return JSON.parse(dadosJSON)
+   }
+   catch (e) {
+      return []
+   }
+
+    
+}
+
+
+
+module.exports = { acessarNotas: acessarNotas, adicionarNota: adicionarNota }
+
